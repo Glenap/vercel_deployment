191aefdebcfa846e9084053ecab21e65d43ed9cf
Vikash Yadav <vikashjiyadav0581@gmail.com>
2025-01-15 23:33:48 +0530

    changes backend dir setup

--- backend/api/index.js
+++ backend/api/index.js
@@ -2,8 +2,8 @@ const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const dotenv = require("dotenv");
-const courseRoutes = require("./routes/courses");
-const authRoutes = require("./routes/auth"); // Import authentication routes
+const courseRoutes = require("../routes/courses");
+const authRoutes = require("../routes/auth"); // Import authentication routes
 
 dotenv.config();
 
